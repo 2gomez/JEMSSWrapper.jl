b4afb7884231d4365ed33179817bde7015ef51f5
2gomez <gomez.seb.lt@proton.me>
2025-10-05 12:53:42 +0200

    Fix docs deployment for unregistered package

--- docs/make.jl
+++ docs/make.jl
@@ -1,14 +1,21 @@
-using Documenter, JEMSSWrapper
+push!(LOAD_PATH, joinpath(@__DIR__, "..", "src"))
+
+using Documenter
+using JEMSSWrapper 
 
 makedocs(
     sitename = "JEMSSWrapper.jl",
     modules = [JEMSSWrapper],
+    format = Documenter.HTML(
+        prettyurls = true,
+    ),
     pages = [
         "Home" => "index.md",
-    ]
+    ],
+    remotes = nothing,
 )
 
 deploydocs(
     repo = "github.com/2gomez/JEMSSWrapper.jl.git",
-    devbranch = "main"
+    devbranch = "main",
 )
